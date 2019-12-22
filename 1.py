f7ae62b9a0c5748660d645558c1204e515483108
gavrishekaterina <ketrin2303@yandex.ru>
2019-12-22 20:37:52 +0400

    создание окна

--- 1.py
+++ 1.py
@@ -1,2 +1,29 @@
-gane = 1
-print(1)
+import pygame
+import os
+
+pygame.init()
+size = width, height = 300, 300
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+# завершение работы:
+pygame.quit()
