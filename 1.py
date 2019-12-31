fa14368a52d10fa2f641f319abc8f2eee64a587d
gavrishekaterina <ketrin2303@yandex.ru>
2019-12-31 13:01:38 +0300

    buttons images, code structure

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import pygame
 import os
 
 pygame.init()
-size = width, height = 300, 300
+size = width, height = 900, 700
 screen = pygame.display.set_mode(size)
 
 
@@ -18,12 +18,38 @@ def load_image(name, colorkey=None):
     return image
 
 
+class Player(pygame.sprite.Group):
+    pass
+
+
+class Buck(pygame.sprite.Group):
+    pass
+
+
+class Stairs(pygame.sprite.Group):
+    pass
+
+
+class Platforms(pygame.sprite.Group):
+    pass
+
+
+class Monster(pygame.sprite.Group):
+    pass
+
+
+
+player = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255, 255, 255))
+    screen.fill((30, 30, 30))
     pygame.display.flip()
 # завершение работы:
 pygame.quit()
